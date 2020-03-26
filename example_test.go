84aa616fa288a8f8a65f0d2e002507f5c342c108
Yingrong Zhao <yingrong.zhao@gmail.com>
2020-03-26 18:36:39 -0400

    add an example test
    Change-Id: I8fccc33c9823261bede6879b0da2d9214bdb00dc

---
+++ example_test.go
@@ -0,0 +1,9 @@
+// Copyright (C) 2020 Storj Labs, Inc.
+// Copyright (C) 2014 Space Monkey, Inc.
+// See LICENSE for copying informatio
+
+package jaeger
+
+import "testing"
+
+func TestExample(t *testing.T) {}
