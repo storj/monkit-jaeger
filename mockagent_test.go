27d5db3c2171f044f48debddc8c97f51ff2d08f5
Yingrong Zhao <yingrong.zhao@gmail.com>
2020-03-27 15:07:07 -0400

    add tests for udp collector
    Change-Id: Ifbb9b9180014d4ced7f5e48c258d9d4f76d15c3e

---
+++ mockagent_test.go
@@ -0,0 +1,101 @@
+// Copyright (C) 2020 Storj Labs, Inc.
+// Copyright (C) 2014 Space Monkey, Inc.
+// See LICENSE for copying information.
+
+package jaeger
+
+import (
+	"context"
+	"net"
+	"sync"
+	"sync/atomic"
+
+	"github.com/apache/thrift/lib/go/thrift"
+
+	"storj.io/monkit-jaeger/gen-go/agent"
+	"storj.io/monkit-jaeger/gen-go/jaeger"
+)
+
+// StartMockAgent starts a mock agent on a local udp port.
+func StartMockAgent(hostPort string) (*MockAgent, error) {
+	addr, err := net.ResolveUDPAddr("udp", hostPort)
+	if err != nil {
+		return nil, err
+	}
+	conn, err := net.ListenUDP(addr.Network(), addr)
+	if err != nil {
+		return nil, err
+	}
+	var w sync.WaitGroup
+	mock := &MockAgent{
+		batches: make([]*jaeger.Batch, 0),
+		mux:     &sync.Mutex{},
+		conn:    conn,
+		addr:    conn.LocalAddr().String(),
+		started: &w,
+	}
+
+	w.Add(1)
+	go mock.serve()
+	w.Wait()
+	return mock, err
+}
+
+// MockAgent implements jaeger agent interface.
+type MockAgent struct {
+	conn    *net.UDPConn
+	addr    string
+	closed  uint32
+	mux     *sync.Mutex
+	batches []*jaeger.Batch
+	started *sync.WaitGroup
+}
+
+// EmitBatch implements jaeger agent interface.
+func (m *MockAgent) EmitBatch(ctx context.Context, batch *jaeger.Batch) (err error) {
+	m.mux.Lock()
+	defer m.mux.Unlock()
+	m.batches = append(m.batches, batch)
+	return nil
+}
+
+// GetBatches returns batches jaeger agent received.
+func (m *MockAgent) GetBatches() []*jaeger.Batch {
+	m.mux.Lock()
+	defer m.mux.Unlock()
+	batches := make([]*jaeger.Batch, len(m.batches))
+	copy(batches, m.batches)
+	return batches
+}
+
+// Addr returns the address of the agent.
+func (m *MockAgent) Addr() string {
+	return m.addr
+}
+
+// Close shutdown mock agent server.
+func (m *MockAgent) Close() error {
+	atomic.StoreUint32(&m.closed, 1)
+	return m.conn.Close()
+}
+
+func (m *MockAgent) serve() {
+	handler := agent.NewAgentProcessor(m)
+	protocolFact := thrift.NewTCompactProtocolFactory()
+	trans := thrift.NewTMemoryBufferLen(maxPacketSize)
+	buf := make([]byte, maxPacketSize)
+	m.started.Done()
+
+	for !m.isClosed() {
+		n, err := m.conn.Read(buf)
+		if err == nil {
+			trans.Write(buf[:n])
+			protocol := protocolFact.GetProtocol(trans)
+			_, _ = handler.Process(context.Background(), protocol, protocol)
+		}
+	}
+}
+
+func (m *MockAgent) isClosed() bool {
+	return atomic.LoadUint32(&m.closed) == 1
+}
---
+++ udp_test.go
@@ -0,0 +1,74 @@
+// Copyright (C) 2020 Storj Labs, Inc.
+// Copyright (C) 2014 Space Monkey, Inc.
+// See LICENSE for copying information.
+
+package jaeger
+
+import (
+	"testing"
+	"time"
+
+	"github.com/spacemonkeygo/monkit/v3"
+	"github.com/stretchr/testify/require"
+
+	"storj.io/monkit-jaeger/gen-go/jaeger"
+)
+
+func TestUDPCollector(t *testing.T) {
+	agentAdrr := "127.0.0.1:0"
+	mock, err := StartMockAgent(agentAdrr)
+	require.NoError(t, err)
+	defer func() {
+		err := mock.Close()
+		require.NoError(t, err)
+	}()
+	collector, err := NewUDPCollector(mock.Addr(), 200, "test", nil)
+	require.NoError(t, err)
+
+	span := &jaeger.Span{
+		TraceIdLow:    monkit.NewId(),
+		SpanId:        monkit.NewId(),
+		OperationName: "test-udp-collector",
+		StartTime:     time.Now().UnixNano() / 1000,
+		Duration:      time.Second.Microseconds(),
+	}
+	collector.Collect(span)
+
+	data := mock.GetBatches()
+	for len(data) < 1 {
+		time.Sleep(1 * time.Millisecond)
+		data = mock.GetBatches()
+	}
+	require.Len(t, data, 1)
+	require.Len(t, data[0].GetSpans(), 1)
+	receivedSpan := data[0].GetSpans()[0]
+	require.Equal(t, span.GetOperationName(), receivedSpan.OperationName)
+	require.Equal(t, span.GetTraceIdLow(), receivedSpan.GetTraceIdLow())
+	require.Equal(t, span.GetSpanId(), receivedSpan.GetSpanId())
+}
+
+func TestUDPCollector_HugeSpan(t *testing.T) {
+	agentAdrr := "127.0.0.1:0"
+	mock, err := StartMockAgent(agentAdrr)
+	require.NoError(t, err)
+	defer func() {
+		err := mock.Close()
+		require.NoError(t, err)
+	}()
+	collector, err := NewUDPCollector(mock.Addr(), 50, "test", nil)
+	require.NoError(t, err)
+
+	span := &jaeger.Span{
+		TraceIdLow:    monkit.NewId(),
+		SpanId:        monkit.NewId(),
+		OperationName: "test-udp-collector",
+		StartTime:     time.Now().UnixNano() / 1000,
+		Duration:      time.Second.Microseconds(),
+	}
+
+	collector.Collect(span)
+
+	time.Sleep(1 * time.Millisecond)
+	data := mock.GetBatches()
+	require.Len(t, data, 0)
+}
